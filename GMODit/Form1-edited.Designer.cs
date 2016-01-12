ffd69e9301935d7332771e86f7b7b934fd414497
Joel Trauger <traugej@techs-iMac.local>
2016-01-12 13:15:59 -0600

    Initial Commit
    Create repo and add code.

---
+++ GMODit/Form1-edited.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace GMODit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.InternetCheck = new System.Windows.Forms.CheckBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.rememberMeBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MODit GUI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(10, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(10, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(13, 47);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(12, 85);
+            this.password.Name = "password";
+            this.password.PasswordChar = 'x';
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 4;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Launch Arcadia Pack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // InternetCheck
+            // 
+            this.InternetCheck.AutoSize = true;
+            this.InternetCheck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.InternetCheck.Location = new System.Drawing.Point(13, 141);
+            this.InternetCheck.Name = "InternetCheck";
+            this.InternetCheck.Size = new System.Drawing.Size(118, 17);
+            this.InternetCheck.TabIndex = 8;
+            this.InternetCheck.Text = "I have slow internet";
+            this.InternetCheck.UseVisualStyleBackColor = false;
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Exit.Location = new System.Drawing.Point(273, 3);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 9;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // rememberMeBox
+            // 
+            this.rememberMeBox.AutoSize = true;
+            this.rememberMeBox.Location = new System.Drawing.Point(119, 87);
+            this.rememberMeBox.Name = "rememberMeBox";
+            this.rememberMeBox.Size = new System.Drawing.Size(94, 17);
+            this.rememberMeBox.TabIndex = 10;
+            this.rememberMeBox.Text = "Remember me";
+            this.rememberMeBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(351, 185);
+            this.Controls.Add(this.rememberMeBox);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.InternetCheck);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GMODit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox InternetCheck;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.CheckBox rememberMeBox;
+    }
+}
+
---
+++ GMODit/Form1-edited.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.Runtime.InteropServices; // This is to unzip the files downloaded from Dropbox
+
+namespace GMODit
+{
+    public partial class Form1 : Form
+    {
+        
+        #region Mouse Capture Code
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.password.KeyUp += new KeyEventHandler(password_KeyUp); //necessary for Enter key capture.
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown); //necessary for mouse capture.
+
+            if (Directory.Exists(@"C:\GMODit\.minecraft") == false)
+            {
+                Directory.CreateDirectory(@"C:\GMODit\.minecraft");
+            }
+
+            if (File.Exists(@"C:\GMODit\.minecraft\gmoditlastlogin") == false)
+            {
+                StreamWriter URaDummyJoel = new StreamWriter(@"C:\GMODit\.minecraft\gmoditlastlogin"); // This creates a blank file
+                URaDummyJoel.Close(); //I'm a dummy because the lack of this section would cause a crash on the first run. Every. Time.
+            }
+
+            StreamReader loginInfo = new StreamReader(@"C:\GMODit\.minecraft\gmoditlastlogin");
+
+            string username;
+
+            if ((username = loginInfo.ReadLine()) != null)
+            {
+                this.rememberMeBox.Checked = true;
+            }
+            string password = loginInfo.ReadLine();
+
+            //decode password string using decryption algorithm that I haven't written yet... :'(
+
+            this.username.Text = username;
+            this.password.Text = password;
+
+            loginInfo.Close();
+
+            
+        }
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rememberMeBox.Checked == true)
+            {
+                StreamWriter logininfo = new StreamWriter(@"C:\GMODit\.minecraft\gmoditlastlogin");
+                logininfo.WriteLine(this.username.Text);
+                logininfo.WriteLine(this.password.Text);
+
+                logininfo.Close();
+            }
+            else
+            {
+                StreamWriter logininfo = new StreamWriter(@"C:\GMODit\.minecraft\gmoditlastlogin"); // creates blank file; overwrites old file
+                logininfo.Close();
+            }
+
+            bool ismodded = false;
+            bool clientExists = false;
+            string folderlocation = @"C:\GMODIT\.minecraft";
+            string modlocation = folderlocation + @"\mods";
+            string coremodlocation = folderlocation + @"\coremods";
+            string templocation = "c:\\temp\\";
+
+            if (Directory.Exists(folderlocation))
+            {
+                clientExists = true;
+            }
+
+            if (!Directory.Exists(templocation))
+            {
+                Directory.CreateDirectory(templocation);
+            }
+
+            if (File.Exists(folderlocation + "\\version3.modit"))
+            {
+                ismodded = true;
+            }
+            else
+            {
+                //download and extract the three files
+
+                if (InternetCheck.Checked == true) //Secret features being planned!
+                {
+                    WebClient downloader = new WebClient();
+                    label4.Text = "Downloading client";
+                    this.Update();
+                    downloader.DownloadFile("<link to file to download>", templocation + "client.zip");
+                    label4.Text = "Downloading Mods";
+                    this.Update();
+                    downloader.DownloadFile("<link to file to download>", templocation + "mods.zip");
+                    label4.Text = "Downloading Coremods";
+                    this.Update();
+                    downloader.DownloadFile("<link to file to download>", templocation + "coremods.zip");
+
+                    label4.Text = "Extracting...";
+                    Shell32.Shell sc = new Shell32.Shell();
+                    Directory.CreateDirectory(@"C:\temp\.minecraft");
+                    Shell32.Folder output = sc.NameSpace(@"C:\temp\.minecraft\");
+                    Shell32.Folder input = sc.NameSpace(templocation + "client.zip");
+                    output.CopyHere(input.Items(), 256);
+
+                    Directory.CreateDirectory(@"C:\temp\.minecraft\mods");
+                    output = sc.NameSpace(@"C:\temp\.minecraft\mods");
+                    input = sc.NameSpace(templocation + "mods.zip");
+                    output.CopyHere(input.Items(), 256);
+
+                    Directory.CreateDirectory(@"C:\temp\.minecraft\coremods");
+                    output = sc.NameSpace(@"C:\temp\.minecraft\coremods");
+                    input = sc.NameSpace(templocation + "coremods.zip");
+                    output.CopyHere(input.Items(), 256);
+
+                    label4.Text = "Cleaning up...";
+                    this.Update();
+                    File.Delete(templocation + "client.zip");
+                    File.Delete(templocation + "mods.zip");
+                    File.Delete(templocation + "coremods.zip");
+                }
+                else
+                {
+                    WebClient downloader = new WebClient();
+                    label4.Text = "Downloading full client. May take a while";
+                    this.Update();
+                    downloader.DownloadFile("<link to file to download>", templocation + "client_full.zip");
+
+                    label4.Text = "Extracting...";
+                    Shell32.Shell sc = new Shell32.Shell();
+                    Directory.CreateDirectory(@"C:\temp\.minecraft");
+                    Shell32.Folder output = sc.NameSpace(@"C:\temp\.minecraft\");
+                    Shell32.Folder input = sc.NameSpace(templocation + "client_full.zip");
+                    output.CopyHere(input.Items(), 256);
+
+                    label4.Text = "Cleaning up...";
+                    File.Delete(templocation + "client_full.zip");
+                }
+
+                new Microsoft.VisualBasic.Devices.Computer().FileSystem.CopyDirectory("C:\\temp\\.minecraft", folderlocation, true);
+
+                Directory.Delete("C:\\temp\\.minecraft", true);
+
+                clientExists = true;
+                ismodded = true;
+            }
+
+            if (clientExists && ismodded)
+            {
+                bool authed = false;
+                string user, session = "";
+                string[] res = verifyUser(username.Text, password.Text, out authed);
+                if (authed)
+                {
+                    user = res[2];
+                    session = res[3];
+                    StartMinecraft(user, session);
+                    Application.Exit();
+                }
+            }
+        }
+
+        private string[] verifyUser(string username, string password, out bool authed)
+        {
+            HttpWebResponse response;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://login.minecraft.net/?user=" + username + "&password=" + password + "&version=13");
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (Exception)
+            {
+                authed = false;
+                return null;
+            }
+            string str = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            if (str.Contains(":") == false)
+            {
+                authed = false;
+                this.label4.Text = str;
+                return null;
+            }
+            authed = true;
+            return str.Split(new char[] { ':' });
+        }
+
+        private void StartMinecraft(string username, string session)
+        {
+            Environment.SetEnvironmentVariable("APPDATA", "C:\\GMODIT\\");
+
+            Process process = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            string dir = @"C:\GMODIT\.minecraft\bin\";
+            info.FileName = "javaw";
+            info.CreateNoWindow = true;
+            info.Arguments = "-cp \"" + dir + "minecraft.jar;" + dir + "lwjgl.jar;" + dir + "lwjgl_util.jar;" + dir + "jinput.jar;\" ";
+            info.Arguments += "\"-Djava.library.path=" + dir + "natives\" -Xmx1024M -Xms512M net.minecraft.client.Minecraft " + username + " " + session;
+            process.StartInfo = info;
+            process.Start();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            if (password.Text == "")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void password_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1_Click(null, null);
+        }
+    }
+}
